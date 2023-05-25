271ff15be79a8e5671b3d81e1d8507ba4ae5ea5d
Jesús Meza <jame181185@gmail.com>
2023-05-24 19:28:46 -0700

    Se agregan íconos.

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,37 @@
   <div class="container">
     <div class="row m-3">
       <div class="col input-group">
-        <span class="input-group-text">Mensaje a encriptar:</span>
+        <span class="input-group-text">
+          <i class="bi-safe"></i>
+          &nbsp;Mensaje a encriptar:
+        </span>
         <input type="text" class="form-control" v-model="textToEncrypt" @keyup.enter="encryptMessage">
       </div>
     </div>
     <div class="row m-3">
       <div class="col input-group">
-        <span class="input-group-text">Mensaje encriptado:</span>
+        <span class="input-group-text">
+          <i class="bi-safe-fill"></i>
+          &nbsp;Mensaje encriptado:
+        </span>
         <input type="text" class="form-control" v-model="cryptedMessage" readonly>
       </div>
     </div>
     <div class="row m-3">
       <div class="col input-group">
-        <span class="input-group-text">Mensaje a desencriptar:</span>
+        <span class="input-group-text">
+          <i class="bi-safe2-fill"></i>
+          &nbsp;Mensaje a desencriptar:
+        </span>
         <input type="text" class="form-control" v-model="textToDecrypt" @keyup.enter="decryptMessage">
       </div>
     </div>
     <div class="row m-3">
       <div class="col input-group">
-        <span class="input-group-text">Mensaje desencriptado:</span>
+        <span class="input-group-text">
+          <i class="bi-safe2"></i>
+          &nbsp;Mensaje desencriptado:
+        </span>
         <input type="text" class="form-control" v-model="decryptedMessage" readonly>
       </div>
     </div>
